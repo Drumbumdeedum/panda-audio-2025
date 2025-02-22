c1e2c6687eb659dcd6173b6a59e621904aa17202
Daniel Szalay <sza.dan.uj@gmail.com>
2025-02-22 08:29:34 +0100

    add basic about and downloads page

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import PageHeader from "@/components/layout/page-header";
+import React from "react";
+
+function AboutPage() {
+  return (
+    <>
+      <PageHeader
+        title="About us"
+        subtitle="Get to know the people behind Panda Audio"
+      />
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ app/downloads/page.tsx
@@ -0,0 +1,15 @@
+import PageHeader from "@/components/layout/page-header";
+import React from "react";
+
+function DownloadsPage() {
+  return (
+    <>
+      <PageHeader
+        title="Downloads"
+        subtitle="Firmware, software, and manuals for all Panda Audio products"
+      />
+    </>
+  );
+}
+
+export default DownloadsPage;
