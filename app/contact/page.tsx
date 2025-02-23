1adf2d582f0904f871191c74313ee230cef27bd6
Daniel Szalay <sza.dan.uj@gmail.com>
2025-02-23 12:32:54 +0100

    add basic contact info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,6 +8,16 @@ function ContactPage() {
         title="Contact us"
         subtitle="Get in touch - we're here to help"
       />
+      <div className="px-4 md:px-8 lg:px-16 py-8 space-y-8">
+        <section>
+          <h4>Contact info</h4>
+          <p>Panda Audio Kft.</p>
+          <p>2040 Budaörs,</p>
+          <p>Széchenyi u. 8.</p>
+          <p>Hungary Tel: +36 20 3693280</p>
+          <p>Email: info@panda-audio.com</p>
+        </section>
+      </div>
     </>
   );
 }
--- components/layout/page-header.tsx
+++ components/layout/page-header.tsx
@@ -7,16 +7,10 @@ type PageHeaderProps = {
 
 function PageHeader({ title, subtitle }: PageHeaderProps) {
   return (
-    <header className="relative overflow-hidden bg-foreground py-12">
-      <div className="relative">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto lg:mx-0 ">
-            <h1 className="tracking-tight mb-0 text-background">{title}</h1>
-            <p className="text-lg leading-none text-background/70">
-              {subtitle}
-            </p>
-          </div>
-        </div>
+    <header className="bg-foreground py-12">
+      <div className="px-4 md:px-8 lg:px-16">
+        <h1 className="tracking-tight mb-0 text-background">{title}</h1>
+        <p className="text-lg leading-none text-background/70">{subtitle}</p>
       </div>
     </header>
   );
