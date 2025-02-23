5b8b72de789d1e29b07726bef7e1750c9012ff08
Daniel Szalay <sza.dan.uj@gmail.com>
2025-02-23 11:15:45 +0100

    update list font color outside of strong tag

--- app/(products)/future-impact/page.tsx
+++ app/(products)/future-impact/page.tsx
@@ -85,72 +85,108 @@ function FutureImpactPage() {
         <section>
           <h2>New Features for V4</h2>
           <ul className="list-disc space-y-2 pl-4">
-            <li>
-              <strong>CV/Gate output</strong> - Simultaneously play a 1V/octave
-              analog synth using the pedal
-            </li>
-            <li>
-              <strong>Detented Parameter dial with extended range</strong> -
-              More variations on each sound
-            </li>
-            <li>
-              <strong>Sturdier knobs</strong> - The potentiometer shafts are now
-              made from solid metal
-            </li>
-            <li>
-              <strong>Long Release</strong> - Audio-triggered sounds now use the
-              full release time of the ADSR
-            </li>
-            <li>
-              <strong>Oscillator sync</strong> - VCO2 can sync to VCO1 for more
-              harmonically complex sounds
-            </li>
-            <li>
-              <strong>Improved audio fidelity</strong> - Via an ultra-low-noise
-              CoDec and noise-reduction coding
-            </li>
-            <li>
-              <strong>Overwrite programs</strong> - On-pedal edits can be saved
-              to patch data before uploading
-            </li>
-            <li>
-              <strong>Superior pitch tracking</strong> - Greatly reduced latency
-              and increased sensitivity to input instrument dynamics. Pitch
-              detection can now be set to prioritize speed or accuracy
-            </li>
-            <li>
-              <strong>Octave transposition</strong> - Two new MIDI CCs to
-              transpose the pitch by ±2 octaves
-            </li>
-            <li>
-              <strong>63 new Flexi curves</strong> - Create a split point
-              between any two adjacent semitones
-            </li>
-            <li>
-              <strong>Robust firmware update process</strong> - Now any MIDI
-              interface can be used
-            </li>
-            <li>
-              <strong>Alternative quick access to Global Menu</strong> -
-              Enter/exit menu without power cycling
-            </li>
-            <li>
-              <strong>Better Global Menu layout</strong> - Items are
-              repositioned and more meaningfully labelled
-            </li>
-            <li>
-              <strong>Fresh Editor look</strong> - An updated color scheme with
-              a clearer and more logical layout
-            </li>
-            <li>
-              <strong>Onscreen Mod Wheel</strong> - Send out MIDI CCs or
-              Aftertouch directly from the Editor
-            </li>
-            <li>
-              <strong>midiBeam 4Control integration</strong> - MIDI signals from
-              expression pedals and switches are automatically passed to the FI4
-              when connected to the Editor
-            </li>
+            <ul className="space-y-2">
+              <li>
+                <strong>CV/Gate output</strong> -{" "}
+                <span className="text-foreground/70">
+                  Simultaneously play a 1V/octave analog synth using the pedal
+                </span>
+              </li>
+              <li>
+                <strong>Detented Parameter dial with extended range</strong> -{" "}
+                <span className="text-foreground/70">
+                  More variations on each sound
+                </span>
+              </li>
+              <li>
+                <strong>Sturdier knobs</strong> -{" "}
+                <span className="text-foreground/70">
+                  The potentiometer shafts are now made from solid metal
+                </span>
+              </li>
+              <li>
+                <strong>Long Release</strong> -{" "}
+                <span className="text-foreground/70">
+                  Audio-triggered sounds now use the full release time of the
+                  ADSR
+                </span>
+              </li>
+              <li>
+                <strong>Oscillator sync</strong> -{" "}
+                <span className="text-foreground/70">
+                  VCO2 can sync to VCO1 for more harmonically complex sounds
+                </span>
+              </li>
+              <li>
+                <strong>Improved audio fidelity</strong> -{" "}
+                <span className="text-foreground/70">
+                  Via an ultra-low-noise CoDec and noise-reduction coding
+                </span>
+              </li>
+              <li>
+                <strong>Overwrite programs</strong> -{" "}
+                <span className="text-foreground/70">
+                  On-pedal edits can be saved to patch data before uploading
+                </span>
+              </li>
+              <li>
+                <strong>Superior pitch tracking</strong> -{" "}
+                <span className="text-foreground/70">
+                  Greatly reduced latency and increased sensitivity to input
+                  instrument dynamics. Pitch detection can now be set to
+                  prioritize speed or accuracy
+                </span>
+              </li>
+              <li>
+                <strong>Octave transposition</strong> -{" "}
+                <span className="text-foreground/70">
+                  Two new MIDI CCs to transpose the pitch by ±2 octaves
+                </span>
+              </li>
+              <li>
+                <strong>63 new Flexi curves</strong> -{" "}
+                <span className="text-foreground/70">
+                  Create a split point between any two adjacent semitones
+                </span>
+              </li>
+              <li>
+                <strong>Robust firmware update process</strong> -{" "}
+                <span className="text-foreground/70">
+                  Now any MIDI interface can be used
+                </span>
+              </li>
+              <li>
+                <strong>Alternative quick access to Global Menu</strong> -{" "}
+                <span className="text-foreground/70">
+                  Enter/exit menu without power cycling
+                </span>
+              </li>
+              <li>
+                <strong>Better Global Menu layout</strong> -{" "}
+                <span className="text-foreground/70">
+                  Items are repositioned and more meaningfully labelled
+                </span>
+              </li>
+              <li>
+                <strong>Fresh Editor look</strong> -{" "}
+                <span className="text-foreground/70">
+                  An updated color scheme with a clearer and more logical layout
+                </span>
+              </li>
+              <li>
+                <strong>Onscreen Mod Wheel</strong> -{" "}
+                <span className="text-foreground/70">
+                  Send out MIDI CCs or Aftertouch directly from the Editor
+                </span>
+              </li>
+              <li>
+                <strong>midiBeam 4Control integration</strong> -{" "}
+                <span className="text-foreground/70">
+                  MIDI signals from expression pedals and switches are
+                  automatically passed to the FI4 when connected to the Editor
+                </span>
+              </li>
+            </ul>
           </ul>
         </section>
         <hr className="my-4 border-t-2 border-border" />
