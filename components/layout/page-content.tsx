d45d87deae095f7a95817f1130a2b8dfd747666c
Daniel Szalay <sza.dan.uj@gmail.com>
2025-02-24 12:28:54 +0100

    adjust page content max-w and auto margin

--- components/layout/page-content.tsx
+++ components/layout/page-content.tsx
@@ -10,10 +10,11 @@ function PageContent({ type = "default", children }: PageContentProps) {
   return (
     <div
       className={clsx(
+        "max-w-[1440] mx-auto",
         `${
           type === "default"
-            ? "p-4 md:p-8 lg:p-16 space-y-8"
-            : "px-4 md:px-24 xl:px-48 py-4 md:py-16 space-y-8 max-w-[1400] mx-auto"
+            ? "p-4 md:p-8 lg:p-16 space-y-8 "
+            : "px-4 md:px-24 xl:px-48 py-4 md:py-16 space-y-8"
         } `
       )}
     >
