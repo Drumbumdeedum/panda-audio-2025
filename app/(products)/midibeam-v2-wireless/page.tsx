d4e3dc083c8d73607590ce1a39f12542db9d179a
Daniel Szalay <sza.dan.uj@gmail.com>
2025-02-25 12:53:14 +0100

    add basic midiBeam v2 wireless page layout

--- app/(products)/midibeam-v2-wireless/page.tsx
+++ app/(products)/midibeam-v2-wireless/page.tsx
@@ -1,7 +1,17 @@
+import PageContent from "@/components/layout/page-content";
+import PageHeader from "@/components/layout/page-header";
 import React from "react";
 
 function MidibeamV2WirelessPage() {
-  return <div>MidibeamV2WirelessPage</div>;
+  return (
+    <>
+      <PageHeader
+        title="midiBeam v2 Wireless"
+        subtitle="Professional wireless MIDI interface"
+      />
+      <PageContent type="narrow">Coming soon...</PageContent>
+    </>
+  );
 }
 
 export default MidibeamV2WirelessPage;
