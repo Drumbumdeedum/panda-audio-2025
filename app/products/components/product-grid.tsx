1015251d10bdb773b5aba8d29f09d11aa87d05cc
Daniel Szalay <sza.dan.uj@gmail.com>
2025-02-25 19:06:09 +0100

    remove unused index

--- app/products/components/product-grid.tsx
+++ app/products/components/product-grid.tsx
@@ -4,7 +4,7 @@ import ProductCard from "./product-card";
 export default async function ProductGrid() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-      {PRODUCTS.map((product, i) => (
+      {PRODUCTS.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>
